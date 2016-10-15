55efe4a652198f3d0158c573fa835b53f28ed26f
vivian.mzh@gmail.com <vivian.mzh@gmail.com>
2016-10-15 09:19:47 -0700

    added ajax and type of request(GET), api url, callback functions if success or fail

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 $(document).ready(function() {
   $('#searchbutton').on('click', function() {
     var searchbox = $('#searchbox').val();
+    $.ajax({
+      type: "GET",
+      url: "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch="+searchbox+"&titles=Main%20Page&prop=revisions&rvprop=content&format=json&origin=*",
+      contentType: 'text/plain',
+      xhrFields: {
+        withCredentials: false
+      },
+      success: function(data) {
+        var results = data.query.search; // [obj, obj, obj...]
+        results.forEach(function(result) {
+          var wikilink = "<a href='https://en.wikipedia.org/wiki/'+result.title></a>";
+          var title = "<div class='wikititle'>"+result.title+"</div>";
+          var snippet = "<div class='wikicontent'>"+result.snippet+"</div>";
+        })
+      },
+      error: function() {
+        console.log('error');
+      }
+    })
   })
   $("#searchbox").keydown(function(key) {
     if (key.keyCode == 13) {
