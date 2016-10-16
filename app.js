090435f7eaed37c00eaffb867dedf119279efb9f
vivian.mzh@gmail.com <vivian.mzh@gmail.com>
2016-10-16 08:31:12 -0700

    refractored code

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 $(document).ready(function() {
-  $('#searchbutton').on('click', function() {
-    var searchbox = $('#searchbox').val();
+  $("#searchbutton").on("click", function() {
+    var searchbox = $("#searchbox").val();
     $.ajax({
       type: "GET",
       url: "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch="+searchbox+"&titles=Main%20Page&prop=revisions&rvprop=content&format=json&origin=*",
-      contentType: 'text/plain',
+      contentType: "text/plain",
       xhrFields: {
         withCredentials: false
       },
       success: function(data) {
         var results = data.query.search; // [obj, obj, obj...]
         results.forEach(function(result) {
-          var wikilink = "<a href='https://en.wikipedia.org/wiki/'+result.title></a>";
-          // var title = "<div class='wikititle'>"+result.title+"</div>";
+          var wikilink = "<a href="https://en.wikipedia.org/wiki/"+result.title></a>";
+          // var title = "<div class="wikititle">"+result.title+"</div>";
           var snippet = "<div class='wikicontent'>"+result.snippet+"</div>";
-          var contenturl = "https://en.wikipedia.org/wiki/"+result.title.split(' ').join('_');
-          $('.wikibox').append("<br>"+"<a href="+contenturl+">"+result.title+"</a>"+snippet);
-          // $('.wikititle').append("<br>"+"<a href="+contenturl+">"+result.title+"</a>");
-          // $('.wikicontent').append("<div>"+result.snippet+"</div>");
+          var contenturl = "https://en.wikipedia.org/wiki/"+result.title.split(" ").join("_");
+          $(".wikibox").append("<br>"+"<a href="+contenturl+">"+result.title+"</a>"+snippet);
+          // $(".wikititle").append("<br>"+"<a href="+contenturl+">"+result.title+"</a>");
+          // $(".wikicontent").append("<div>"+result.snippet+"</div>");
         })
       },
       error: function() {
-        console.log('error');
+        console.log("error");
       }
     })
   })
